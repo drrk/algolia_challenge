59d368c0bf1ad9b8cef7ff558ca5252aae3bcac8
Kimball Johnson <kimball.johnson@konghq.com>
2024-01-25 17:48:32 +0000

    Add example config file with dummy data
    Signed-off-by: Kimball Johnson <kimball.johnson@konghq.com>

---
+++ config.example.php
@@ -0,0 +1,7 @@
+<?php
+
+// Update the values here and rename this file to config.php
+
+define("APP_ID",     "<<App ID>");
+define("API_ADMIN_KEY",    "<<API Admin Key>>");
+define("API_SEARCH_KEY", "<<API Search Key>>"); // Currently Unused in Loader Script
